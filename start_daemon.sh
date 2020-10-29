66209d9ee8c0d43957b9df07df32db77f5a3cc26
Sergey <52304443+c29r3@users.noreply.github.com>
2020-10-30 02:33:47 +0300

    Update start_daemon.sh

--- start_daemon.sh
+++ start_daemon.sh
@@ -3,7 +3,7 @@
 cd /root/spacemesh/ && \
  ./spacemesh --grpc-server --json-server --tcp-port 7153 --config ./tn01.json \
  -d ./sm_data \
- --coinbase $SPACEMESH_ADDRESS \
+ --coinbase $(cat /root/spacemesh/address.txt) \
  --start-mining \
  --post-datadir ./post_data \
  --metrics
