6e1304c1ab564fcace5b1834680ec153cfbe4fd7
Sergey <52304443+c29r3@users.noreply.github.com>
2020-10-30 01:45:50 +0300

    Update start_daemon.sh

--- start_daemon.sh
+++ start_daemon.sh
@@ -3,7 +3,7 @@
 cd /root/spacemesh/ && \
  ./spacemesh --grpc-server --json-server --tcp-port 7153 --config ./tn01.json \
  -d ./sm_data \
- --coinbase $(addr=$(cat accounts.json | jq -r .ss.pubkey); echo 0x${addr:24}) \
+ --coinbase $SPACEMESH_ADDRESS \
  --start-mining \
  --post-datadir ./post_data \
  --metrics
