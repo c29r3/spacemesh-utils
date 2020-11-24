44a73fa2ae0f71d6a036ff34073f648dd20ba613
Sergey <52304443+c29r3@users.noreply.github.com>
2020-11-25 02:00:09 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ UNIT_FILE_URL="https://raw.githubusercontent.com/c29r3/spacemesh-utils/main/spac
 START_FILE="https://raw.githubusercontent.com/c29r3/spacemesh-utils/main/start_daemon.sh"
 SERVICE_NAME="spacemesh_node.service"
 
+systemctl stop $SERVICE_NAME
 mkdir $HOME_PATH/spacemesh
 cd $HOME_PATH/spacemesh
 
@@ -15,11 +16,10 @@ wget $RELEASE_URL -O spacemesh && chmod u+x spacemesh
 wget $CLI_URL -O cli && chmod u+x cli
 wget $CONFIG_URL -O tn01.json
 wget $UNIT_FILE_URL -O /etc/systemd/system/$SERVICE_NAME
+systemctl daemon-reload
 wget $START_FILE -O start_daemon.sh && chmod u+x start_daemon.sh
 
-systemctl stop $SERVICE_NAME
 rm -rf sm_data post_data
-systemctl daemon-reload
 systemctl enable $SERVICE_NAME
 systemctl start $SERVICE_NAME
 
